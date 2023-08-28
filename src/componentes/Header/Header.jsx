2bae34d58c65fe7a7c0c52ee0d65e3f3be8196e1
Manuel Zimmermann <manuzimmermann@Manuels-MacBook-Pro.local>
2023-08-28 12:03:26 -0300

    footer y header

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,9 +1,34 @@
 import React from "react"
 
 function Header (props){
-    // return(
+    return(
+        <header>
+      <article class="logo">
+        <a href="index.html"><img src="./img/Vibes.gif" alt="logo" /></a>
+      </article>
+
+      <nav>
+        <ul class="links">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="genres.html">Géneros</a></li>
+          <li><a href="playlist.html">Favoritos</a></li>
+        </ul>
+      </nav>
+
+      
+      <article class="all-search">
+        <section class="search">
+          <form action="search-results.html" method="GET" class="formIndex">
+            <input type="text" class="busq" name="buscador" value="" />
+            <button class="bSub" type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </form>
+        </section>
+      </article>
+    </header>
         
-    // )
+    )
 }
 
 export default Header
