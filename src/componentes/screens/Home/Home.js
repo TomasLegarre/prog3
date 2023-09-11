159be7fae3d701745e2043742ca2faade00c6b49
TomasLegarre <tlegarrematera@udesa.edu.ar>
2023-09-11 09:09:00 -0300

    comit

--- src/componentes/screens/Home/Home.js
+++ src/componentes/screens/Home/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
         }
 
         controlarCambios(evento) {
-            this.setState({valor: evento.target.value});
+            this.setState({valor: evento.target.value}); // aca tenemos el valor de la busqueda guardado. 
         }
 
         render() {
@@ -29,8 +29,6 @@ class Home extends Component {
     
                     <h2>Home</h2>
         
-    
-                     
                     <form className="search" onSubmit={(evento) => this.evitarSubmit(evento)} >
                             <label>Busqueda:</label>
                             <input type="text" onChange={(evento)=>this.controlarCambios(evento)} value={this.state.valor}/>
