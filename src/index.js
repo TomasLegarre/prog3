f7a1bec410aed6f585832d456d0a42899c75fc3b
Manuel Zimmermann <zimmermannm@udesa.edu.ar>
2023-09-25 21:54:57 -0300

    cambio

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //renderizamos el div
 root.render(
-<BrowserRouter> // enrutador general y necesario para administrar las rutas del proyecto.
+<BrowserRouter> 
     <App />
 </BrowserRouter>
 );
 
+// enrutador general y necesario para administrar las rutas del proyecto.
